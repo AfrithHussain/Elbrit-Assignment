deae1f1df3f2fa68149b6f4c9bbc5ad1e59d1563
AfrithHussain <afrithhussain64@gmail.com>
2024-12-22 18:43:04 +0530

    Image name corrected

--- src/Pages/LogoComponent.jsx
+++ src/Pages/LogoComponent.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "../assets/logo.png";
+import Logo from "../assets/Logo.png";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 
